ff6eff7fdeb443fb0a52b0ffc6da377846300027
hari-lr <156157882+hari-lr@users.noreply.github.com>
2024-02-25 00:15:37 -0500

    Create 02-clean_data.R

---
+++ scripts/02-clean_data.R
@@ -0,0 +1,6 @@
+#### Preamble ####
+# Purpose: Clean the Australian Prime Minister data set from Wikipedia. 
+# Author: Hari Lee Robledo
+# Date: 24 February 2024
+# Email: hari.leerobledo@mail.utoronto.ca
+# Pre-requisites: none

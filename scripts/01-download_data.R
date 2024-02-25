5ecac1a616ec82d0db571f8f66cd5743cc33300f
hari-lr <156157882+hari-lr@users.noreply.github.com>
2024-02-25 00:15:07 -0500

    Update 01-download_data.R

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,3 +1,10 @@
+#### Preamble ####
+# Purpose: Download the Australian Prime Minister data set from Wikipedia. 
+# Author: Hari Lee Robledo
+# Date: 24 February 2024
+# Email: hari.leerobledo@mail.utoronto.ca
+# Pre-requisites: none
+
 ### Work Space Setup ###
 
 library(babynames)
